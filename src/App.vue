34ccf1694c772d42d11e5334f0e34c5d6cbf6ccf
evelyncaraujo <evyaraujoifc@gmail.com>
2023-03-30 18:49:14 -0300

    commit-01

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from "vue";
+const name = ref(`seu nome `)
+const idade = ref(0);
+
+const email =ref(``);
+
+</script>
+<template>
+  <h1>Formulários</h1>
+  <br>
+  <div>
+    <label>Informe seu nome:</label>
+    <input type="text" v-model="name" />
+  </div>
+  <br>
+  <div>
+    <label>Informe sua idade:</label>
+    <input type="number" v-model="idade" />
+   
+  </div>
+  <br>
+  <div>
+    <label for="email">E-mail:</label>
+    <input type="email" v-model="email">
+   
+  </div>
+  <br>
+  <div class="button">
+    <button @click="submitForm">Enviar</button>
+</div>
+
+
+</template>
+
+<style>
+h1{
+  text-align: center;
+  font-size: 20px;
+  
+}
+</style>
