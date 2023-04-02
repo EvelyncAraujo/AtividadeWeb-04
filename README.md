aa8a281f3b94081d787addfdf058c3c44b260c67
evelyncaraujo <evyaraujoifc@gmail.com>
2023-04-01 21:34:30 -0300

    commit-02

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# atvd04
+# atvd
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import { ref } from "vue";
 const name = ref(`seu nome `)
 const idade = ref(0);
-
 const email =ref(``);
 
 </script>
 <template>
   <h1>Formulários</h1>
   <br>
+  <label class="fundo" >
+    <br>
+
   <div>
     <label>Informe seu nome:</label>
     <input type="text" v-model="name" />
@@ -22,21 +24,39 @@ const email =ref(``);
   <br>
   <div>
     <label for="email">E-mail:</label>
-    <input type="email" v-model="email">
+    <input type="email" v-model="email" />
    
   </div>
   <br>
-  <div class="button">
-    <button @click="submitForm">Enviar</button>
-</div>
+  <div>
+    <button type="submit">Enviar</button>
+  </div>
+<br>
 
+</label>
 
 </template>
 
 <style>
+
 h1{
   text-align: center;
-  font-size: 20px;
-  
+  font-size: 20px;  
+  font-family: Arial, Helvetica, sans-serif;
+
 }
+.fundo{
+  text-align: center;
+  line-height: 10px;
+  background-color: rgb(152, 127, 163);
+  border-radius: 8px;
+}
+input{
+  border-radius: 5px;
+  border: none;
+}
+button{
+  text-align: center
+}
+
 </style>
